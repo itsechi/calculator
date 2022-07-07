0e116e81f821d0e5baa8e2502a8b8abb135cd09a
itsechi <wardulinskao@gmail.com>
2022-07-07 13:50:22 +0200

    Add clear, point and divide by zero functions

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const operatorBtns = document.querySelectorAll('.operators');
 const numberBtns = document.querySelectorAll('.numbers');
 const equalsBtn = document.querySelector('.equal');
-const removeBtn = document.querySelector('.remove');
+const clearBtn = document.querySelector('.clear');
+const allClearBtn = document.querySelector('.allClear');
 const displayDiv = document.querySelector('.display');
 const displayDivSmall = document.querySelector('.displaySmall');
+const pointBtn = document.querySelector('.point');
 let a = '';
 let b = '';
 let operator = '';
 let result = '';
 let isEqual = false;
 
+numberBtns.forEach(button => button.addEventListener('click', assignNumber));
 
-numberBtns.forEach((button) =>
-  button.addEventListener('click', assignNumber)
-);
-
-operatorBtns.forEach((button) =>
+operatorBtns.forEach(button =>
   button.addEventListener('click', assignOperator)
 );
 
@@ -23,29 +22,37 @@ equalsBtn.addEventListener('click', () => {
   isEqual = true;
   operate();
 });
-removeBtn.addEventListener('click', reset);
 
+allClearBtn.addEventListener('click', allClear);
+
+clearBtn.addEventListener('click', clear);
+
+pointBtn.addEventListener('click', assignNumber);
 
 // store user selected numbers
 function assignNumber(e) {
-  if (operator !== '') { 
-    b += e.target.textContent; 
-    displayDiv.textContent = b; 
-    } else { 
-    a += e.target.textContent; 
+  if (operator !== '') {
+    // if (b < 999999999999999999n)
+    pointBtn.addEventListener('click', assignNumber);
+    if (b.includes('.')) pointBtn.removeEventListener('click', assignNumber);
+    b += e.target.textContent;
+    displayDiv.textContent = b;
+  } else {
+    // if (a < 999999999999999999n)
+    if (a.includes('.')) pointBtn.removeEventListener('click', assignNumber);
+    a += e.target.textContent;
     displayDiv.textContent = a;
-    }
+  }
 }
-
 
 // store user selected operators
 function assignOperator(e) {
   operate();
-  if (e.target.textContent === '+') { 
-    operator = '+'; 
-    b = ''; 
+  if (e.target.textContent === '+') {
+    operator = '+';
+    b = '';
     displayDivSmall.textContent = `${a} ${operator}`;
-    displayDiv.textContent = a; 
+    displayDiv.textContent = a;
   } else if (e.target.textContent === '-') {
     operator = '-';
     b = '';
@@ -57,83 +64,108 @@ function assignOperator(e) {
     displayDivSmall.textContent = `${a} ${operator}`;
     displayDiv.textContent = a;
   } else if (e.target.textContent === '/') {
-    operator = '/';
-    b = '';
-    displayDivSmall.textContent = `${a} ${operator}`;
-    displayDiv.textContent = a;
-  } 
-  
+    if (result === Infinity) {
+      divideZero();
+    } else {
+      operator = '/';
+      b = '';
+      displayDivSmall.textContent = `${a} ${operator}`;
+      displayDiv.textContent = a;
+    }
+  }
 }
 
+function clear() {
+  if (operator !== '' && result === '') {
+    b = b.slice(0, -1);
+    displayDiv.textContent = b;
+  } else if (operator == '') {
+    a = a.slice(0, -1);
+    displayDiv.textContent = a;
+  } else return;
+}
 
 // clear calc
-function reset() {
-  a = ''; 
+function allClear() {
+  a = '';
   b = '';
   result = '';
   operator = '';
   isEqual = false;
   displayDiv.textContent = '';
   displayDivSmall.textContent = '';
+  pointBtn.addEventListener('click', assignNumber);
 }
 
+function divideZero() {
+  displayDiv.textContent = `Can't divide by 0!`;
+  b = '';
+  return;
+}
 
 // math functions
 function add(a, b) {
-  return result = a + b;
+  return (result = a + b);
 }
 
 function subtract(a, b) {
-  return result = a - b;
+  return (result = a - b);
 }
 
 function multiply(a, b) {
-  return result = a * b;
+  return (result = a * b);
 }
 
 function divide(a, b) {
-  return result = a / b;
+  return (result = a / b);
 }
-
 
 // calculator function
 function operate() {
-  if (b === '') { 
+  if (b === '') {
     return;
   }
-    a = Number(a);
-    b = Number(b);
-  
-    if (operator === '+') {
-      add(a, b);
-      displayDiv.textContent = b;
-      displayDivSmall.textContent = `${a} ${operator} ${b} =`;
-    } else if (operator === '-') {
-      subtract(a, b);
-      displayDiv.textContent = b;
-      displayDivSmall.textContent = `${a} ${operator} ${b} =`;
-    } else if (operator === '*') {
-      multiply(a, b);
-      displayDiv.textContent = b;
-      displayDivSmall.textContent = `${a} ${operator} ${b} =`;
-    } else if (operator === '/') {
-      divide(a, b);
-      displayDiv.textContent = b;
-      displayDivSmall.textContent = `${a} ${operator} ${b} =`;
-    } else {
-      displayDiv.textContent = a;
-    }
 
-    if (result !== '' && !isEqual) {
+  a = Number(a);
+  b = Number(b);
+
+  if (operator === '+') {
+    add(a, b);
+    displayDiv.textContent = b;
+    displayDivSmall.textContent = `${a} ${operator} ${b} =`;
+  } else if (operator === '-') {
+    subtract(a, b);
+    displayDiv.textContent = b;
+    displayDivSmall.textContent = `${a} ${operator} ${b} =`;
+  } else if (operator === '*') {
+    multiply(a, b);
+    displayDiv.textContent = b;
+    displayDivSmall.textContent = `${a} ${operator} ${b} =`;
+  } else if (operator === '/') {
+    divide(a, b);
+    displayDiv.textContent = b;
+    displayDivSmall.textContent = `${a} ${operator} ${b} =`;
+  } else {
+    displayDiv.textContent = a;
+  }
+
+  if (result !== '' && !isEqual) {
+    if (result === Infinity) {
+      divideZero();
+    } else {
       a = result;
       result = '';
       operator = '';
     }
+  }
 
-    if (isEqual) {
+  if (isEqual) {
+    if (b === 0) {
+      divideZero();
+    } else {
       displayDiv.textContent = result;
       displayDivSmall.textContent = `${a} ${operator} ${b} =`;
       isEqual = false;
     }
+  }
 }
-
